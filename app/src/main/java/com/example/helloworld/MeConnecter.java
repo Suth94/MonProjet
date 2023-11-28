2982cb44baae340981f11f5c071a95040888a375
Sutharsan <sutharsan.gobalakrishnan@edu.esiee.fr>
2023-11-28 20:01:58 +0100

    Rassemblement des 2 commits précédents

--- app/src/main/java/com/example/helloworld/MeConnecter.java
+++ app/src/main/java/com/example/helloworld/MeConnecter.java
@@ -1,19 +1,18 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.ArrayList;
-
 public class MeConnecter extends AppCompatActivity {
-
     private TextView connect;
     private TextView password;
     private TextView email;
@@ -21,6 +20,10 @@ public class MeConnecter extends AppCompatActivity {
     private EditText motdepasse;
     private EditText adresse;
 
+
+    private Button seconnecter;
+
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,16 @@ public class MeConnecter extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.MDP);
         connect = findViewById(R.id.Seconnecter);
+        seconnecter = findViewById(R.id.button_se_connecter);
+        register = findViewById(R.id.button_register);
         getSupportActionBar().setTitle("Se Connecter");
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MeConnecter.this,creationcompte.class));
+            }
+        });
 
 
     }
